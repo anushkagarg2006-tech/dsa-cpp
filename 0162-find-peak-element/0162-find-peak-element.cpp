42dc05aac92aefcc58d2c6446886dd9b31a1f7d5
Anushka Garg <anushkagarg2006@gmail.com>
2026-06-27 18:13:57 +0530

    Time: 0 ms (100%), Space: 12.6 MB (39.39%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -11,7 +11,7 @@ public:
                 if (m == 0)
                     break;
                 else if (nums[m] < nums[m - 1]) {
-                    e = m;
+                    e = m-1;
                 } else {
                     return m;
                 }
