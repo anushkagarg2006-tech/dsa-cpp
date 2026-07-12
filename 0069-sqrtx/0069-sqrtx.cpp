08fc907d16e9787425197e8e37774695cd325695
Anushka Garg <anushkagarg2006@gmail.com>
2026-07-12 14:38:05 +0530

    Time: 0 ms (100%), Space: 8.4 MB (87.22%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int s=0;
+        int e=x;
+        while(s<=e){
+            long long mid=s+(e-s)/2;
+            if(mid*mid==x) return mid;
+            else if(mid*mid<x) s=mid+1;
+            else{e=mid-1;}
+        }return e;
+    }
+};
