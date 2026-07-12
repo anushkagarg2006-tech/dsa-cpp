66ba3a64747bd8d7836912bb3458ee3ec34521ba
Anushka Garg <anushkagarg2006@gmail.com>
2026-07-12 14:43:54 +0530

    Time: 0 ms (100%), Space: 7.8 MB (53.74%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        int s=0;
+        int e=num;
+        while(s<=e){
+            long long mid=s+(e-s)/2;
+            if(mid*mid==num) return true;
+            else if(mid*mid<num) s=mid+1;
+            else{e=mid-1;}
+        }return false;
+    }
+};
